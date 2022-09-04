7253667bf2ee1c2a2b91b72e1d3340c79c0de199
SinthujanBalachandran <54698571+SinthujanBalachandran@users.noreply.github.com>
2022-09-04 22:15:29 +0530

    initial commit2

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,30 @@
 const menu = document.querySelector('.menu')
 const navItems = document.querySelector('.nav--items')
-const body = document.querySelector('.body')
+const navItem0 = document.querySelector('.item-0')
+const navItem1 = document.querySelector('.item-1')
+const navItem2 = document.querySelector('.item-2')
+const navItem3 = document.querySelector('.item-3')
+const navItem4 = document.querySelector('.item-4')
+const navItem5 = document.querySelector('.item-5')
+const header = document.querySelector('.header')
+
 
 menu.onclick = ()=>{
-    navItems.classList.toggle("active--menu");
+    header.classList.toggle("active--menu");
 }
+
+navItem0.onclick = ()=> header.classList.remove("active--menu");
+navItem1.onclick = ()=> header.classList.remove("active--menu");
+navItem2.onclick = ()=> header.classList.remove("active--menu");
+navItem3.onclick = ()=> header.classList.remove("active--menu");
+navItem4.onclick = ()=> header.classList.remove("active--menu");
+navItem5.onclick = ()=> header.classList.remove("active--menu");
+
+
+function scrollHeader() {
+    const header = document.querySelector('.header')
+    if (this.scrollY >= 100) header.classList.add('scroll-header');
+    else header.classList.remove('scroll-header')
+}
+
+window.addEventListener('scroll', scrollHeader)
